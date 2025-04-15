3d7109b7433c8b2e65fc8bb2e0ace1936bcb1df6
Erik Tomisaki Rodrigues da Silva <erik.trs01@gmail.com>
2025-04-15 17:58:24 -0300

    Added API libraries and JWT Authentication Library

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -45,4 +46,26 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /**
+     * Get the JWT identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier(): mixed
+    {
+        // Return the primary key of the user
+        return $this->getKey();
+    }
+
+    /**
+     * Get custom claims for the JWT.
+     *
+     * @return array<string, mixed>
+     */
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
+
 }
