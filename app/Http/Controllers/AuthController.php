5cbe83fd39928592db452ed74d9947c77866cf9a
Erik Tomisaki Rodrigues da Silva <erik.trs01@gmail.com>
2025-04-19 18:42:31 -0300

    Fixed methods in Authentication Controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,17 +7,20 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
 
     public function login(Request $request)
     {
+        // Validate the request
         $request->validate([
             'email' => 'required|string|email',
             'password' => 'required|string',
         ]);
 
+        // If the credentials are invalid, return an error response
         $credentials = $request->only('email', 'password');
 
         if (! $token = auth()->attempt($credentials)) {
@@ -26,33 +29,47 @@ class AuthController extends Controller
             ], 401);
         }
 
-        $user = auth()->user();
+        // If authentication is successful, get the user
+        // $user = User::where('email', $request->email)->first();
+        $user = auth()->userOrFail();
 
+        // Check if the user password is valid
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'status' => '401',
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        // Return the user and token
         return response()->json([
             'status' => 'success',
-            'user' => $user,
+            'user' => $user->only('name', 'email'),
             'authorization' => [
                 'token' => $token,
                 'type' => 'bearer',
+                'expires_in' => auth()->factory()->getTTL() * 60
             ]
-        ]);
+        ], 202);
     }
 
     public function register(Request $request)
     {
+        // Validate the request
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
 
+        // Create the user
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        $token = Auth::login($user);
+       $token = auth()->login($user);;
 
         return response()->json([
             'status' => 'success',
@@ -67,7 +84,11 @@ class AuthController extends Controller
 
     public function logout()
     {
+        // Logout the user
         Auth::logout();
+
+        // Invalidate the token
+        auth()->invalidate();
 
         return response()->json([
             'status' => 'success',
@@ -77,12 +98,14 @@ class AuthController extends Controller
 
     public function refresh()
     {
+        // Refresh the token
         return response()->json([
             'status' => 'success',
-            'user' => Auth::user(),
+            'user' => auth()->user(),
             'authorization' => [
-                'token' => Auth::refresh(),
+                'token' => auth()->refresh(),
                 'type' => 'bearer',
+                'expires_in' => auth()->factory()->getTTL() * 60
             ]
         ]);
     }
