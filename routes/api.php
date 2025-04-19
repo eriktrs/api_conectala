5f8b8608c57d3f4188888ee767a06fc07ef802a6
Erik Tomisaki Rodrigues da Silva <erik.trs01@gmail.com>
2025-04-19 18:43:16 -0300

    Added new routes in API

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\JwtMiddleware;
 
 // API Routes
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
+// Authentication routes
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware([JwtMiddleware::class])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+// User routes
+Route::middleware([JwtMiddleware::class])->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
 });
